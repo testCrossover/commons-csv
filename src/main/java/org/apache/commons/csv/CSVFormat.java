7e626180af770be37ed824a0d62d6087fd473c92
Kiran <kiran.borole07@gmail.com>
2017-10-10 18:26:24 +0530

    Reduced iteration & sleep time

--- src/main/java/org/apache/commons/csv/CSVFormat.java
+++ src/main/java/org/apache/commons/csv/CSVFormat.java
@@ -796,7 +796,7 @@ public final class CSVFormat implements Serializable {
         for (int i=0; i<5; i++) {
             System.out.print('.');
             try {
-                Thread.currentThread().sleep(1000);
+                Thread.currentThread().sleep(5);
             } catch (InterruptedException e) {
                 break;
             }
